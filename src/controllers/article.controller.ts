42d03096cbc82a45fe2890973ffcff6545d564ed
Pratiyush Kumar <pratiyush.inv@gmail.com>
2025-01-17 20:38:10 +0530

    Post Like and dislike feature

--- src/controllers/article.controller.ts
+++ src/controllers/article.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from 'express';
 import { Post } from '@/db/models/article.model.ts';
 import { Users } from '@/db/models/users.model.ts';
+import { Reaction } from '@/db/models/reaction.model.ts';
 
 export const createArticle = async (
   req: Request,
@@ -111,5 +112,81 @@ export const deleteArticle = async (req: Request, res: Response) => {
     res
       .status(500)
       .json({ message: 'Server error: Something went wrong!', error });
+  }
+};
+
+export const likeDislikeArticle = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { article_id } = req.params;
+
+    console.log('article id in controller', article_id);
+
+    const articleFromPost = await Post.getArticleById(article_id);
+    if (!articleFromPost) {
+      res.status(404).json({ message: 'Article not found' });
+      return;
+    }
+
+    const { reaction_type } = req.body;
+    const article = await Reaction.getReactionByPostId(article_id);
+
+    if (!article) {
+      const reactionCreation = await Reaction.createReaction(
+        { article_id },
+        reaction_type
+      );
+
+      res.status(200).json({
+        message: 'Reaction created successfully',
+        reactionCreation,
+      });
+      return;
+    }
+
+    const updateReaction = await Reaction.updatePostLikeOrDislike(
+      article_id,
+      reaction_type
+    );
+    res.status(200).json({
+      message: 'Reaction updated successfully',
+      updateReaction,
+    });
+    return;
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Server error: Something went wrong!', error });
+    return;
+  }
+};
+
+export const getArticleReactionDetails = async (
+  req: Request,
+  res: Response
+) => {
+  const { article_id } = req.params;
+
+  try {
+    const articleReactionDetails =
+      await Reaction.getReactionByPostId(article_id);
+
+    if (!articleReactionDetails) {
+      res.status(404).json({ message: 'Article not found' });
+      return;
+    }
+
+    res.status(200).json({
+      message: 'Reaction details fetched successfully',
+      articleReactionDetails,
+    });
+    return;
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Server error: Something went wrong!', error });
+    return;
   }
 };
